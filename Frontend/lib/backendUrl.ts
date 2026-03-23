7099535e8e6b7e350d1289561136015598db52d5
Vini719-code <chouhanvinita0510@gmail.com>
2026-03-23 12:59:07 +0530

    backend url

--- Frontend/lib/backendUrl.ts
+++ Frontend/lib/backendUrl.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "https://blog-generator-l1v5.onrender.com";
+const baseURL = "https://blog-generator-l1v5.onrender.com";
 
 const backendUrl = axios.create({
   baseURL,
